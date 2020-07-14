407a30a42fd8e245e3ba893d5da3ba11f4e016aa
Saurabh Saxena <sau.5rabh88@gmail.com>
2020-07-14 21:28:59 +0530

    Update shell script

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,11 +7,10 @@
   mkdir -p /var/log/proto
   touch /var/log/proto/access.log
   touch /var/log/proto/application.log
-  touch /var/log/proto/error.log
   
   exec gunicorn --bind "0.0.0.0:5000" app.io.slacklife:flask_app \
-#    --capture-output \
-#    --error-logfile /var/log/proto/error.log \
-#    --log-file /var/log/proto/application.log \
-#    --access-logfile /var/log/proto/access.log \
-#    --log-level info
+    --log-syslog \
+    --capture-output \
+    --log-file /var/log/proto/application.log \
+    --access-logfile /var/log/proto/access.log \
+    --log-level info
